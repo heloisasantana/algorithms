751692c667b864878e41bc5bb7fb07a00285f85a
heloisasantana <heloisasantana.silva@hotmail.com>
2023-03-26 22:26:28 -0300

    Requisito 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,17 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    ord_first_string = "".join(selection_sort(list(first_string.casefold())))
+    ord_second_string = "".join(selection_sort(list(second_string.casefold())))
+    boolean = ord_first_string == ord_second_string
+    return (ord_first_string, ord_second_string, boolean)
+
+
+def selection_sort(string):
+    for letter in range(len(string) - 1):
+        min_letter = letter
+        for index in range(letter + 1, len(string)):
+            if string[index] < string[min_letter]:
+                min_letter = index
+        current_letter = string[letter]
+        string[letter] = string[min_letter]
+        string[min_letter] = current_letter
+    return string
